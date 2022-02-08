47a68974c941c6ae13d1cf0354d3854895cdfbce
Ugo Palatucci <upalatuc@redhat.com>
2022-02-08 18:27:57 +0100

    fix rollup css

--- rollup.config.js
+++ rollup.config.js
@@ -24,6 +24,7 @@ export default [
       },
     ],
     plugins: [resolve(), commonjs(), postcss(), typescript({ tsconfig: './tsconfig.json' })],
+    external: ['react', 'react-dom'],
   },
   {
     input: 'dist/esm/types/src/index.d.ts',
